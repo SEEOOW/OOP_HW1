99b8dad534a3402e892f6c2d4e50bc03831ecc9c
Fedor Sazhin <fedorsazhin96@gmail.com>
2024-03-20 14:10:40 +0200

    final HW_4 task 1

--- main.py
+++ main.py
@@ -2,28 +2,27 @@ class Category:
     total_categories = 0
     total_unique_products = 0
 
-    def __init__(self, title, description, products=[]):
+    def __init__(self, title, description, products=None):
         self.title = title
         self.description = description
-        self.__goods = list(set(products))  # делаем список товаров приватным и уникальным
+        if products is None:
+            products = []
+        self.__goods = list(set(products))
         Category.total_categories += 1
         Category.total_unique_products += len(self.__goods)
 
-    # метод добавления товаров
     def add_product(self, product):
         self.__goods.append(product)
         Category.total_unique_products += 1
 
-    # Геттер для атрибута класса Category товары
     @property
     def goods(self):
         return self.__goods
 
-    # метод для отображения списка товаров
     def display_list_products(self):
         result = ''
         for product in self.__goods:
-            result += f'{product.title}, {product.price} руб. Остаток: {product.quantity} шт.\n'
+            result += f'{product}\n'
         return result
 
     def __len__(self):
@@ -64,3 +63,26 @@ class Product:
             return self.price * self.quantity + other.price * other.quantity
         else:
             raise TypeError("Unsupported operand type(s) for +: 'Product' and '{}'".format(type(other)))
+
+
+class Smartphone(Product):
+    def __init__(self, title, description, price, quantity, performance, model, memory_capacity, color):
+        super().__init__(title, description, price, quantity)
+        self.performance = performance
+        self.model = model
+        self.memory_capacity = memory_capacity
+        self.color = color
+
+    def __str__(self):
+        return f"{super().__str__()}, Производительность: {self.performance}, Модель: {self.model}, Объем памяти: {self.memory_capacity}, Цвет: {self.color}"
+
+
+class LawnGrass(Product):
+    def __init__(self, title, description, price, quantity, country_of_origin, germination_period, color):
+        super().__init__(title, description, price, quantity)
+        self.country_of_origin = country_of_origin
+        self.germination_period = germination_period
+        self.color = color
+
+    def __str__(self):
+        return f"{super().__str__()}, Страна-производитель: {self.country_of_origin}, Срок прорастания: {self.germination_period}, Цвет: {self.color}"
